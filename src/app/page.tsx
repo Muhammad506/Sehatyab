6f9b491b642b7d8df1c2e8d5716d747fcce1134b
Muhammad506 <muhammadumersaleem408@gmail.com>
2025-02-18 02:35:48 +0500

    Ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import NavBar from './component/Navbar'
+import Hero from './component/Hero'
+import ExpertiseBanner from './component/ExpertierBarrier'
+import MentalHealthDirectory from './component/MentalHealthDirectory'
+import ServicesForKids from './component/ServicesForKids'
+import GetQuote from './component/GetQuote'
+import OurConsultants from './component/Consultant'
+import Footer from './component/Footer'
 
 
 const page = () => {
   return (
     <div>
-
+      <NavBar />
+      <Hero />
+      <ExpertiseBanner />
+      <MentalHealthDirectory />
+      <ServicesForKids />
+      <GetQuote/>
+      <OurConsultants/>
+      <Footer/>
     </div>
   )
 }
